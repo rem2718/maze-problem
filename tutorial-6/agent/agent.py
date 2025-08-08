5baf461e7f7d0273490f9e5985d4a52bf994b2d2
reme2.718 <103392516+rem2718@users.noreply.github.com>
2025-08-09 02:32:40 +0300

    tutorial-6

--- tutorial-6/agent/agent.py
+++ tutorial-6/agent/agent.py
@@ -4,23 +4,17 @@ import numpy as np
 class Agent:
     def __init__(self, env: gym.Env):
         self.env = env
+        self.gamma = self.env.gamma
         self.i = 0
         self.j = 0
-        self.episode = []
-        self.returns = np.full((env.size, env.size), 0, dtype=float)
-        self.counts = np.full((env.size, env.size), 0, dtype=float)
         self.values = np.full((env.size, env.size), np.nan, dtype=float)
         
-    def _V(self, _s=None):
-        R = 0
-        for s, r in reversed(self.episode):
-            x, y = s
-            R += r             
-            self.returns[x][y] += R
-            self.counts[x][y] += 1
-            self.values[x][y] = self.returns[x][y] / self.counts[x][y]
-
-        self.episode = []
+    def _V(self, s):
+        v = self.values[s[0], s[1]]
+        if np.isnan(v):
+            return 0.0
+        return v
+     
     
     def _policy(self, _s):
         trajectories = [[2, 2, 2, 2, 0, 0, 0, 0, 3, 3, 0, 0, 0, 2, 2, 2, 2, 2],
@@ -42,9 +36,8 @@ class Agent:
         return self._policy(s)
 
     def update(self, s, a, r, nxt_s, over):
-        self.episode.append((s, r))
-        if over:
-            self._V()
+        print(r)
+        self.values[s[0], s[1]] = r + self.gamma * self._V(nxt_s)
     
     def get_values(self):
         return self.values
